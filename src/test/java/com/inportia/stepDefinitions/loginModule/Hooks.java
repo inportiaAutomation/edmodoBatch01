09632776989112c79a0c03a62a56feb6debd22f3
inportia <inportia@yopmail.com>
2016-03-12 11:04:32 +0530

    Adding Hooks

--- src/test/java/com/inportia/stepDefinitions/loginModule/Hooks.java
+++ src/test/java/com/inportia/stepDefinitions/loginModule/Hooks.java
@@ -1,5 +1,15 @@
 package com.inportia.stepDefinitions.loginModule;
 
+import com.inportia.utils.BrowserManager;
+
+import cucumber.api.java.After;
+
 public class Hooks {
 
+	  
+	@After
+	public void closeBrowser()
+	{
+		BrowserManager.closeBrowser();
+	}
 }
